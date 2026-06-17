402db73aea0b5d19b8048e208384709ddedf00cc
AT8Cool <leo8gaming@gmail.com>
2026-06-17 22:11:11 +0530

    Containerize application with Docker Compose

--- app/main.py
+++ app/main.py
@@ -21,12 +21,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://shawty.in",
-        "https://www.shawty.in",
-    ],
+  allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:4173",
+    "http://127.0.0.1:4173",
+    "https://shawty.in",
+    "https://www.shawty.in",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- app/services/redis_client.py
+++ app/services/redis_client.py
@@ -5,5 +5,11 @@ Export it for the rest of the app
 """
 
 import redis
+import os
+REDIS_HOST = os.getenv("REDIS_HOST","localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT",6379))
 
-redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+
+redis_client = redis.Redis(host= REDIS_HOST, 
+                           port=REDIS_PORT, 
+                           decode_responses=True)
